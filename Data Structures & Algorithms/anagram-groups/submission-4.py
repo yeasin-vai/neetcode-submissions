124a0d8b54210ecadb60e8b64d66429831ed67ed
Mohammad Yeasin Al Fahad <phonixfahad@gmail.com>
2026-09-23 00:04:53 +0800

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sorted_strs = []
+        # anagrams = []
+        # visited = set()
+        # for word in strs: 
+        #     sorted_strs.append("".join(sorted(word)))
+
+        # for i in range (len(sorted_strs)):
+        #     if i not in visited:
+        #         currentWord = sorted_strs[i]
+        #         subList = [strs[i]]
+                
+        #         for j in range(i + 1, len(sorted_strs)):
+        #             if sorted_strs[j] == currentWord:
+        #                 subList.append(strs[j])
+        #                 visited.add(j)
+        #         anagrams.append(subList)
+        
+        # return anagrams
+
+        # anagram_dict = {}
+        # anagram = []
+
+        # for word in strs: 
+        #     sorted_word = "".join(sorted(word))
+        #     if sorted_word not in anagram_dict: 
+        #         anagram_dict[sorted_word] = [word]
+        #     else:
+        #         print(anagram_dict[sorted_word])
+        #         anagram_dict[sorted_word].append(word)
+        #     print(anagram_dict)
+        #     print('end of iteration')
+
+        
+        # for key in anagram_dict:
+        #     anagram.append(anagram_dict[key])
+        
+        # return anagram
+        anagram_dict = defaultdict(list)
+
+        for word in strs:
+            letters = [0] * 26
+
+            for char in word:
+                letters[ord(char) - ord('a')] += 1
+            
+            anagram_dict[tuple(letters)].append(word)
+
+        return list(anagram_dict.values())
+
+
+
+
+
+
+
+        
